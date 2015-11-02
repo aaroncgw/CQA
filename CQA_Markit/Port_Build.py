d21d91129fa620b4f473ab238900037c8cc5b770
aaroncgw <aaroncgw@gmail.com>
2015-11-02 12:08:55 -0600

    11/2

--- CQA_Markit/Port_Build.py
+++ CQA_Markit/Port_Build.py
@@ -18,39 +18,41 @@ CAPITAL = 10000000
 
 columns = ["Ticker", "Sector", "Name", "Price", "MktCap", "rank_score", "Book_Lever", "EPS_Dis", "Ahead_EPS", "6m_Return", "52w_Trend", "3m_EPS_Dis", "Dollar_Sens", "DS_ratio", "Profit_Assets", "Profit_Margin", "EBITDA_EV", "Sales_Yield"]
 
-secotr_bets_dict = { "Cyclical Goods & Services": 18,
-                     "Energy" : 16,
-                     "Non-Cyclical Goods & Services" : 14,                          
+secotr_bets_dict = { "Cyclical Goods & Services": 20,
+                     "Energy" : 20,
+                     "Industrials" : 16,
+                     "Basic Materials" : 16,
+                     "Non-Cyclical Goods & Services" : 12,                          
                      "Utilities": 12,
-                     "Industrials" : 10,
-                     "Basic Materials" : 10,
                      "Technology" : 6,
-                     "Telecommunication Services" : 6,
-                     "Financials" : 4,
-                     "Healthcare" : 4 }
-
-def weights_calc(data_df, sector, bets, sector_capital):    
+                     "Financials" : 6,
+                     "Telecommunication Services" : 6,                     
+                     "Healthcare" : 6 }                    
+                     
+def portfolio_calc(data_df, sector, bets, sector_capital):    
     sector_df = data_df[data_df["Sector"] == sector].copy()
     sector_df.sort_index(by="score", inplace=True, ascending=False)
     
     buy_df = sector_df.head(int(0.5*bets)).copy()
-    buy_df["Beta_inverse"] = 1 / buy_df["Beta"]
-    s1 = buy_df["Beta_inverse"].sum()
-    buy_df["weights"] =  np.round(1 / (buy_df["Beta"] * s1), 2)
-    buy_df["raw_shares"] = buy_df["weights"] * sector_capital / buy_df["current_price"]
-    buy_df["shares"] = 100 * np.round(buy_df["raw_shares"] * 0.01, 0)
-    buy_df["capital"] = buy_df["current_price"] * buy_df["shares"]
-    
     sell_df = sector_df.tail(int(0.5*bets)).copy()
-    sell_df["Beta_inverse"] = 1 / sell_df["Beta"]
-    s1 = sell_df["Beta_inverse"].sum()
-    sell_df["weights"] =  np.round(1 / (sell_df["Beta"] * s1), 2)
-    sell_df["raw_shares"] = sell_df["weights"] * sector_capital / sell_df["current_price"]
-    sell_df["shares"] = -100 * np.round(sell_df["raw_shares"] * 0.01, 0)
-    sell_df["capital"] = sell_df["current_price"] * sell_df["shares"]
+    buy_sell_df = pd.concat([buy_df, sell_df])
     
-    return pd.concat([buy_df, sell_df])
+    buy_sell_df["Beta_inverse"] = 1 / buy_sell_df["Beta"]
+    s = buy_sell_df["Beta_inverse"].sum()
+    buy_sell_df["weights"] =  buy_sell_df.apply(lambda row: weights_calc(row, s),axis=1)
+    
+    #buy_df["weights"] =  np.round(1 / (buy_df["Beta"] * s1), 2)
+    buy_sell_df["raw_shares"] = buy_sell_df["weights"] * sector_capital / buy_sell_df["current_price"]
+    buy_sell_df["shares"] = 100 * np.round(buy_sell_df["raw_shares"] * 0.01, 0)
+    buy_sell_df["capital"] = buy_sell_df["current_price"] * buy_sell_df["shares"]
+    buy_sell_df["actual_weights"] = buy_sell_df["capital"] / sector_capital
+    return buy_sell_df
 
+def weights_calc(row, s):      
+    weight = 2 / (row["Beta"] * s)    
+    if row["score"] < 0:
+        weight = -weight
+    return weight
 
 def _request(symbols, tags):
     symbol_str = str()        
@@ -103,9 +105,11 @@ buysell_df = pd.DataFrame()
 
 for key, value in secotr_bets_dict.iteritems():
     if value != 0:    
-        buysell_df = pd.concat([buysell_df, weights_calc(df, key, value, 0.01*value*CAPITAL)])
+        buysell_df = pd.concat([buysell_df, portfolio_calc(df, key, value, 0.01*value*CAPITAL)])
         
-buysell_df.to_csv(r"C:\Users\Guanwen\Google Drive\CQA\CQA_Positions.csv")
+
+#buysell_df.groupby("Sector")["actual_weights"].sum()
+#buysell_df.to_csv(r"C:\Users\Guanwen\Google Drive\CQA\CQA_Positions.csv")
 
     
              
